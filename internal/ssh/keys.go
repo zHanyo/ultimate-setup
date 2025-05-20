61cac3407f58ca7d6d9c8d3d2c3f1ba9fac24703
Zha Nyo <zhanyo@protonmail.com>
2025-05-20 05:42:02 +0200

    pipeline13

--- internal/ssh/keys.go
+++ internal/ssh/keys.go
@@ -10,7 +10,7 @@ import (
 	"github.com/AlecAivazis/survey/v2"
 )
 
-func GenerateKey() {
+func GenerateKey(email ...string) {
 	sshPath := filepath.Join(os.Getenv("HOME"), ".ssh", "id_ed25519")
 
 	// Vérifie si une clé existe déjà
@@ -28,16 +28,20 @@ func GenerateKey() {
 		return
 	}
 
-	var email string
-	prompt := &survey.Input{
-		Message: "Enter email for SSH key:",
-	}
-	if err := survey.AskOne(prompt, &email); err != nil {
-		fmt.Println("❌ Failed to get email input:", err)
-		return
+	var userEmail string
+	if len(email) > 0 {
+		userEmail = email[0]
+	} else {
+		prompt := &survey.Input{
+			Message: "Enter email for SSH key:",
+		}
+		if err := survey.AskOne(prompt, &userEmail); err != nil {
+			fmt.Println("❌ Failed to get email input:", err)
+			return
+		}
 	}
 
-	cmd := exec.Command("ssh-keygen", "-t", "ed25519", "-C", email, "-f", sshPath, "-N", "")
+	cmd := exec.Command("ssh-keygen", "-t", "ed25519", "-C", userEmail, "-f", sshPath, "-N", "")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
